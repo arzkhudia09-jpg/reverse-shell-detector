40cc542933054e4ee213eb95a2ed11665fba9f84
arzkhudia09-jpg <arzkhudia09@gmail.com>
2026-07-29 16:26:36 +0530

    made function to read and print pids and their respective processes

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 bool is_numeric(const char *name){
     if (name == NULL){
@@ -18,18 +19,35 @@ bool is_numeric(const char *name){
     return true;
 }
 
+bool read_comm(int pid, char *buffer, size_t size){
+    char path[256];
+    snprintf(path, sizeof(path),  "/proc/%d/comm", pid);
+    FILE *fptr = fopen(path, "r");
+    if (fptr == NULL){
+        perror("Failed to open /proc/");
+        return false;
+    }
+    fgets(buffer, size, fptr);
+    fclose(fptr);
+    printf("%s", buffer);
+    return true;
+}
+
 int main(){
 
-    DIR *dir = opendir("/proc");
+    DIR *dir = opendir("/proc/");
     if (dir == NULL){
-        perror("Failed to open /proc");
+        perror("Failed to open /proc/");
         return 1;
     }
 
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL){
         if (is_numeric(entry->d_name)){
-            printf("Found file: %s\n", entry->d_name);
+            int PID = atoi(entry->d_name);
+            printf("Found file: %d\n", PID);
+            char comm_buffer[256];
+            read_comm(PID, comm_buffer, 256);
         }
     }
 
